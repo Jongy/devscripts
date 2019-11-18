01d5d6ccbff398abe11ff69d269f9f5951828c37
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2019-11-18 18:04:50 +0200

    count_references.py: Print distribution of relocation types, per item

--- count_references.py
+++ count_references.py
@@ -2,6 +2,7 @@ import sys
 
 from elftools.elf.elffile import ELFFile
 from elftools.elf.relocation import RelocationSection
+from elftools.elf.descriptions import describe_reloc_type
 
 
 def main():
@@ -13,6 +14,7 @@ def main():
     item = sys.argv[2] if len(sys.argv) > 2 else ""
 
     counters = {}
+    reloc_types = {}
     elf = ELFFile(open(elf, "rb"))
 
     for sec in elf.iter_sections():
@@ -29,10 +31,20 @@ def main():
                     counters[name] += 1
                 else:
                     counters[name] = 1
+                    reloc_types[name] = {}
 
-    counters = [(v, k) for (k, v) in counters.items()]
-    for count, name in sorted(counters, reverse=True):
+                reloc_type = describe_reloc_type(reloc["r_info_type"], elf)
+                if reloc_type in reloc_types[name]:
+                    reloc_types[name][reloc_type] += 1
+                else:
+                    reloc_types[name][reloc_type] = 1
+
+    counters_list = [(v, k) for (k, v) in counters.items()]
+    for count, name in sorted(counters_list, reverse=True):
         print("{}: {}".format(name, count))
+        reloc_list = [(v, k) for (k, v) in reloc_types[name].items()]
+        for count, reloc_type in sorted(reloc_list, reverse=True):
+            print("\t{}: {}".format(reloc_type, count))
 
 
 if __name__ == "__main__":
